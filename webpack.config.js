ad481353b0589bd42869df629479dc87540e3510
sillah2010 <macktastic2014@gmail.com>
2020-07-05 23:15:52 -0400

    Webpack successfully compiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[hash].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[hash].css',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
 };
